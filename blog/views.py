b0ada6bbaca0c203d18b2acc489b02c5a4b1f884
희은 <datakubit2@gmail.com>
2024-07-01 04:42:15 +0900

    백엔드 세션 준비 완료

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,13 @@ from .models import Post
 from .serializers import PostSerializer
 
 class PostView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def get(self, request, *args, **kwargs):
         post_id = kwargs.get('id')
         if post_id:
@@ -17,13 +24,6 @@ class PostView(APIView):
         posts = Post.objects.filter(is_deleted=False)
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request, *args, **kwargs):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, *args, **kwargs):
         post_id = kwargs.get('id')
